ae2dfe4750273858a68a6afa1397052c00a9c983
Facundo Cipresso <facucipresso@gmail.com>
2022-08-10 21:19:07 -0300

    agregando-librerias

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 let cards = document.getElementById('cards');
 
 const peliculasEnCartelera = [{ id: '1', titulo: 'Minions: nace un villano', imagen: 'minions-1.jpg',alt: 'foto Minions', genero: 'infantil', horario1: 22, horario2: 23, capacidadTotal: 50, capacidadDisponible: 29 }, { id: '2', titulo: 'Thor: amor y trueno', imagen: 'thor-1.jpg' ,alt: 'foto Thor', genero: 'accion', horario1: 20, horario2: 22, capacidadTotal: 50, capacidadDisponible: 25 }, { id: '3', titulo: 'Lightyear', imagen: 'lightyear-1.jpg', alt: 'foto Lightyear', genero: 'infantil', horario1: 19, horario2: 21, capacidadTotal: 50, capacidadDisponible: 40 }, { id: '4', titulo: 'foto Jurassic World: Dominion', imagen: 'jurassic-word-1.jpg', alt:'Jurassic World',  genero: 'accion', horario1: 19, horario2: 22, capacidadTotal: 50, capacidadDisponible: 10 }, { id: '5', titulo: 'The Black Phone', imagen: 'el_telefono_negro-1.jpg' ,alt: 'foto The Black Phone', genero: 'terror', horario1: 23, horario2: 00, capacidadTotal: 50, capacidadDisponible: 19 }, { id: '6', titulo: 'Top Gun: Maverick', imagen: 'top-gun.jpg' ,alt: 'foto Top Gun: Maverick', genero: 'accion', horario1: 21, horario2: 23, capacidadTotal: 50, capacidadDisponible: 2 }
@@ -46,7 +47,17 @@ function mostrarInfo() {
             const indiceElemento = peliculasSeleccionadas.findIndex(item => item.id === idPelicula)
             peliculasSeleccionadas.splice(indiceElemento, 1)
             mostrarInfo();
-
+            Swal.fire({
+                position: 'top-center',
+                icon: 'warning',
+                iconColor: 'red',
+                title: '¿Estas seguro que queres eliminar la pelicula?',
+                showConfirmButton: true,
+                confirmButtonColor: '#b01518',
+                confirmButtonText:'Si, quiero borrar',
+                
+              })
+            
         })
     }
 
@@ -58,26 +69,43 @@ function compraDeEntradas(evento) {
     peliculasSeleccionadas.push(peliculasAgregadas);
     console.log(peliculasSeleccionadas);
     mostrarInfo();
+    Toastify ({
+        text: 'Seleccionaste una pelicula para ver',
+        duration: 1500,
+        style: {
+            background: '#660dbf',
+            color: 'black',
+            width: '15%',
+            height: '10%',
+            
+        },
+        offset:{
+            x:'10%',
+            y:'40%',
+        }
+    }).showToast();
 }
 
 for (var i = 0; i < btnEntradas.length; i++) {
     btnEntradas[i].addEventListener('click', compraDeEntradas);   
 }
 
-
-/*  btnEntradas.addEventListener('click', ()=>{
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Your work has been saved',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    }) 
- */
-
 const peliculasSeleccionadas = []
 
 sessionStorage.setItem('peliculas', JSON.stringify(peliculasSeleccionadas));
 let peliculasRecuperadas = JSON.parse(sessionStorage.getItem('peliculas'));
 console.log(peliculasRecuperadas);
+
+const DateTime = luxon.dateTime;
+
+const ahora = luxon.DateTime.now();
+//console.log(ahora); 
+console.log(ahora.toString());
+console.log(ahora.toLocaleString());
+console.log(ahora.toLocaleString(DateTime.TIME_SIMPLE));
+console.log(ahora.toLocaleString(DateTime.DATE_HUGE));
+
+console.log(ahora.toLocaleString(DateTime.DATE_FULL));
+
+
+
